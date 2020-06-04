1f991b8f3653fe0d62a3b3118121b09fc1a02faa
Dave Miller <github@justdave.net>
2020-06-04 01:48:49 -0400

    Remove unused library include

--- app/src/main/java/net/justdave/mcstatus/ServerListViewAdapter.java
+++ app/src/main/java/net/justdave/mcstatus/ServerListViewAdapter.java
@@ -10,7 +10,6 @@ import android.graphics.Color;
 import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
