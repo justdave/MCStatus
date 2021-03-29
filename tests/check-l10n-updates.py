3da289a93117f91a4c23a1194dc8d514da7731e1
Dave Miller <github@justdave.net>
2021-03-28 22:10:36 -0400

    let languages be tested individually

--- tests/check-l10n-updates.py
+++ tests/check-l10n-updates.py
@@ -11,15 +11,48 @@ GREEN = '\033[92m'
 RED = '\033[91m'
 ENDC = '\033[0m'
 
+l10n_found = {}
+language = '*'
+if len(sys.argv) >= 2:
+    language = sys.argv[1]
+    l10n_found[language] = 1
+
+# localizable_files: array of descriptions of files to test
+#   source: a fileglob that matches a set of files that can be localized
+#   localized: a fileglob that matches the same files, but localized
+#   src_id_re: and
+#   dst_id_re: regexp substitution patterns used to generate a unique
+#       ID based on the pathname of the file. src_id_re is run on the
+#       source file, and dst_re_id is run on the localized file. Both
+#       should result in identical strings, i.e. the source file and its
+#       corresponding localized file should have the same ID. The first
+#       value is a pattern to be matched, the second is a string to replace
+#       the matched part with. It can be a simple search and replace, or
+#       you can you group matches () and backreferences \\1
+#   lang_re: a regexp which containing a group () that matches the language
+#       code in the path of the localized file
 localizable_files = [
-    'playstore/en/*.txt',
-    'app/src/main/res/values/strings.xml',
-    'app/src/main/res/raw/*.txt'
-    ]
-localized_locations = [
-    'playstore/*/*.txt',
-    'app/src/main/res/values-*/strings.xml',
-    'app/src/main/res/raw-*/*.txt'
+    {
+        'source':    'playstore/en/*.txt',
+        'localized': 'playstore/{}/*.txt'.format(language),
+        'src_id_re': ['^playstore/en/', 'playstore/'],
+        'dst_id_re': ['^playstore/[^/]+/', 'playstore/'],
+        'lang_re':   '^playstore/([^/]+)/'
+        },
+    {
+        'source':    'app/src/main/res/values/strings.xml',
+        'localized': 'app/src/main/res/values-{}/strings.xml'.format(language),
+        'src_id_re': ['no-op', 'no-op'], # the original path is fine
+        'dst_id_re': ['^app/src/main/res/values-[^/]+/', 'app/src/main/res/values/'],
+        'lang_re':   '^app/src/main/res/values-([^/]+)/'
+        },
+    {
+        'source':    'app/src/main/res/raw/*.txt',
+        'localized': 'app/src/main/res/raw-{}/*.txt'.format(language),
+        'src_id_re': ['no-op', 'no-op'], # the original path is fine
+        'dst_id_re': ['^app/src/main/res/raw-[^/]+/', 'app/src/main/res/raw/'],
+        'lang_re':   '^app/src/main/res/raw-([^/]+)/'
+        },
     ]
 
 def run_with_output(string):
@@ -37,8 +70,9 @@ if DEBUG:
     print("{}======================{}".format(GREEN,ENDC))
 
 branch = run_with_output('git branch --show-current').strip('\n')
+errors = 0
 for fileglob in localizable_files:
-    output = run_with_output('git ls-files {} | xargs -n1 git log --format=format:"~ %aI" --name-only -n1 {} --'.format(fileglob,branch))
+    output = run_with_output('git ls-files {} | xargs -n1 git log --format=format:"~ %aI" --name-only -n1 {} --'.format(fileglob['source'],branch))
     currentdate = ""
     for line in output.splitlines():
         if DEBUG:
@@ -46,15 +80,19 @@ for fileglob in localizable_files:
         if line[:1] == "~":
             currentdate = datetime.fromisoformat(line[2:])
         else:
-            fileid = line.replace('/en/','/')
+            if DEBUG:
+                print('search: {}'.format(fileglob['src_id_re'][0]))
+                print('search: {}'.format(fileglob['src_id_re'][1]))
+            fileid = re.sub(fileglob['src_id_re'][0],fileglob['src_id_re'][1],line)
             default_files[fileid] = {
                 'filename': line,
-                'commit_date': currentdate
+                'commit_date': currentdate,
+                'l10n_found': {}
                 }
-
-errors = 0
-for fileglob in localized_locations:
-    output = run_with_output('git ls-files {} | grep -v "playstore/en/" | xargs -n1 git log --format=format:"~ %aI" --name-only -n1 {} --'.format(fileglob,branch))
+            if DEBUG:
+                print('fileid: {}'.format(fileid))
+                print(str(default_files[fileid]))
+    output = run_with_output('git ls-files {} | grep -v "playstore/en/" | xargs -n1 git log --format=format:"~ %aI" --name-only -n1 {} --'.format(fileglob['localized'],branch))
     currentdate = ""
     for line in output.splitlines():
         if DEBUG:
@@ -62,8 +100,14 @@ for fileglob in localized_locations:
         if line[:1] == "~":
             currentdate = datetime.fromisoformat(line[2:])
         else:
-            fileid = re.sub('(playstore/)[^/]+/', '\\1', line)
-            fileid = re.sub('(main/res/[^-/]+)-[^/]+', '\\1', fileid)
+            fileid = line
+            lang = '*'
+            m = re.match(fileglob['lang_re'], line)
+            if m:
+                lang = m.group(1)
+            fileid = re.sub(fileglob['dst_id_re'][0], fileglob['dst_id_re'][1], line)
+            default_files[fileid]['l10n_found'][lang] = 1;
+            l10n_found[lang] = 1;
             if (currentdate.timestamp() >= default_files[fileid]['commit_date'].timestamp()):
                 if DEBUG:
                     print("v- localized: {} newer than source: {}".format(currentdate,default_files[fileid]['commit_date']))
@@ -73,6 +117,15 @@ for fileglob in localized_locations:
                     print("v- localized: {} older than source: {}".format(currentdate,default_files[fileid]['commit_date']))
                 errors = errors + 1
                 print("[{}FAIL{}] {} is outdated.".format(RED,ENDC,line))
+
+if DEBUG:
+    print('l10n_found: {}'.format(list(l10n_found.keys())))
+for filename in default_files:
+    for lang in l10n_found:
+        if not lang in default_files[filename]['l10n_found']:
+            print('[{}FAIL{}] {} is not localized for {}.'.format(RED,ENDC,default_files[filename]['filename'],lang))
+            errors = errors + 1
+
 
 if (errors > 0):
     print("{}{} outdated localization files found.{}".format(RED,errors,ENDC))
